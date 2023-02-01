3530dca2e250005acf293db585e351ed4d0dc0a4
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-02-01 03:20:16 -0600

    Fixed e17
    All tests pass. E17 passes test without being a static function.

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,18 +1,32 @@
 export function minBy(array, cb) {
+	if (!array.length) {
+		return undefined;
+	}
+	let minVal = cb(array[0]);
+	let minObj = array[0];
 
-  for(let minAge of array){
-    if (cb(minAge) === 22 ){
-      return minAge
-    }
-  }
-};
+	for (let i = 0; i < array.length; i++) {
+		if (cb(array[i]) < minVal) {
+			minVal = cb(array[i]);
+			minObj = array[i];
+		}
+	}
+
+	return minObj;
+}
 
 export function maxBy(array, cb) {
-  for(let maxAge of array){
-    if (cb(maxAge) === 30){
-      if(maxAge){
-        return maxAge
-      }
-    }
-  }
-};
+	if (!array.length) {
+		return undefined;
+	}
+	let maxVal = cb(array[0]);
+	let maxObj = array[0];
+
+	for (let i = 0; i < array.length; i++) {
+		if (cb(array[i]) > maxVal) {
+			maxVal = cb(array[i]);
+			maxObj = array[i];
+		}
+	}
+	return maxObj;
+}
